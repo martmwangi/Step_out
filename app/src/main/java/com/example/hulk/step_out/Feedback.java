75edfa5435586683bde36af8f32e8eace214ab6b
martmwangi <luthermwangi2@gmail.com>
2015-11-17 18:31:03 +0300

    Starting files for stepout application

---
+++ app/src/main/java/com/example/hulk/step_out/Feedback.java
@@ -0,0 +1,47 @@
+package com.example.hulk.step_out;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.yalantis.guillotine.animation.GuillotineAnimation;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class Feedback extends AppCompatActivity {
+    private static final long RIPPLE_DURATION = 250;
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+    @InjectView(R.id.root)
+    FrameLayout root;
+    @InjectView(R.id.content_hamburger)
+    View contentHamburger;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+            ButterKnife.inject(this);
+
+
+            if (toolbar != null) {
+                setSupportActionBar(toolbar);
+                getSupportActionBar().setTitle(null);
+            }
+
+            View guillotineMenu = LayoutInflater.from(this).inflate(R.layout.guillotine, null);
+            root.addView(guillotineMenu);
+
+            new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+                    .setStartDelay(RIPPLE_DURATION)
+                    .setActionBarViewForAnimation(toolbar)
+                    .setClosedOnStart(true)
+                    .build();
+        }
+    }
