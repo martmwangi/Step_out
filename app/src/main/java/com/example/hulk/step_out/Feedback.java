a50ea8b70313956c135dfe4b5d6d48b0810b0926
martmwangi <luthermwangi2@gmail.com>
2015-11-18 08:40:58 +0300

    Feedback complete

--- app/src/main/java/com/example/hulk/step_out/Feedback.java
+++ app/src/main/java/com/example/hulk/step_out/Feedback.java
@@ -1,11 +1,13 @@
 package com.example.hulk.step_out;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.yalantis.guillotine.animation.GuillotineAnimation;
 
@@ -45,5 +47,51 @@ public class Feedback extends AppCompatActivity {
                 .setActionBarViewForAnimation(toolbar)
                 .setClosedOnStart(true)
                 .build();
+        LinearLayout click1 = (LinearLayout) guillotineMenu.findViewById(R.id.now_showing);
+        click1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Nowshowing.class);
+                startActivity(i);
+
+            }
+        });
+        LinearLayout click2 = (LinearLayout) guillotineMenu.findViewById(R.id.coming_soon);
+        click2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Comingsoon.class);
+                startActivity(i);
+
+            }
+        });
+        LinearLayout click3 = (LinearLayout) guillotineMenu.findViewById(R.id.offers);
+        click3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Offers.class);
+                startActivity(i);
+
+            }
+        });
+        LinearLayout click4 = (LinearLayout) guillotineMenu.findViewById(R.id.contact);
+        click4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Contact.class);
+                startActivity(i);
+
+            }
+        });
+        LinearLayout click5 = (LinearLayout) guillotineMenu.findViewById(R.id.feedback);
+        click5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Feedback.class);
+                startActivity(i);
+
+            }
+        });
+
         }
     }
