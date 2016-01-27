e6478c37be2e13b2cf0381058700f05009f26002
martmwangi <luthermwangi2@gmail.com>
2016-01-27 18:13:56 +0300

    Added simple login lib for facebook

---
+++ app/src/main/java/com/example/hulk/step_out/widget/CanaroTextView.java
@@ -0,0 +1,27 @@
+package com.example.hulk.step_out.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.example.hulk.step_out.App;
+
+
+/**
+ * Created by Dmytro Denysenko on 5/6/15.
+ */
+public class CanaroTextView extends TextView {
+    public CanaroTextView(Context context) {
+        this(context, null);
+    }
+
+    public CanaroTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CanaroTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setTypeface(App.canaroExtraBold);
+    }
+
+}
